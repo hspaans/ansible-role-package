df808859136fbf117c6bf378c0de0b84fc02c250
Hans Spaans <hans@dailystuff.nl>
2020-12-28 13:58:41 +0000

    Adding default scenario

---
+++ molecule/default/tests/test_default.py
@@ -0,0 +1,30 @@
+"""Role testing files using testinfra."""
+
+import pytest
+
+
+def test_hosts_file(host):
+    """Validate /etc/hosts file."""
+    f = host.file("/etc/hosts")
+
+    assert f.exists
+    assert f.user == "root"
+    assert f.group == "root"
+
+
+@pytest.mark.parametrize('pkg', [
+  'nano'
+])
+def test_pkg_installed(host, pkg):
+    package = host.package(pkg)
+
+    assert package.is_installed
+
+
+@pytest.mark.parametrize('pkg', [
+  'joe'
+])
+def test_pkg_not_installed(host, pkg):
+    package = host.package(pkg)
+
+    assert not package.is_installed
